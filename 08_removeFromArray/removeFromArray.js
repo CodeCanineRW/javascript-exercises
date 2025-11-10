058d7624309745eae6d678a610d08f60f3a84685
Ryan Winker <64046649+CodeCanineRW@users.noreply.github.com>
2025-11-09 16:53:47 -0800

    Complete exercises and tests thru 10

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...remove) {
+    // let removeItems = [...remove];
+    // for (let item of removeItems) {
+    //     while (array.find((element) => element === item)) {
+    //     let itemIndex = array.indexOf(item);
+    //     array.splice(itemIndex, 1);               
+    //     }
+    //     }
+    // return array;
+
+    return array.filter((item) => !remove.includes(item));
+}
+let sampleArray = [1, 2, 3, 4, 5, 3, 4, 6, 7, 4];
+console.log(removeFromArray(sampleArray, 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+// Implement a function that takes 2 positive integers and returns the sum of 
+// every integer between (and including) them:
 
+
+const sumAll = function(a, b) {
+    let range = 0;
+    let start;
+    let sum = 0;
+    if (a<0 || b<0 || a === NaN || b === NaN || Math.floor(a) !== a || Math.floor(b) !== b) {
+        return 'ERROR';
+    }
+    if (b >= a) {
+        range = b - a;
+        start = a
+    } else if (a > b) {
+        range = a - b;
+        start = b
+    }
+    for (i = range ; i >= 0 ; i--) {
+        sum += (start + i)
+    }
+    return sum;
 };
+
+console.log(sumAll(10, 5));
 
 // Do not edit below this line
 module.exports = sumAll;
